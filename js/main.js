dcc5715ecd12f0aae6ccb1275dd896f8063023f0
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-10-31 13:25:10 -0500

    sort by hash done

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
   'use strict';
   $(document).ready(initialize);
 
-  var photos, mostPopular, i;
+  var photos, mostPopular, costumes, i;
   var counter = 0;
 
   function initialize(){
@@ -22,19 +22,32 @@
 
   function didLoad(dom){
     $('.instagram').on('didLoadInstagram', function(event, response) {
-      sortPopular(response);
+      console.log(response);
+      sortByHash(response)
+      // sortPopular(response);
       rank(dom);
     });
   }
 
-  function sortPopular(response){
-    mostPopular = [];
+  function sortByHash(response){
+    costumes = [];
     for (i = 0; i < 33; i++){
-      mostPopular.push(response.data[i]);
+      if (response.data[i].tags[0] == "cc" || response.data[i].tags[0] == "nomstreet14"){
+        costumes.push(response.data[i]);
+      }
     };
-    mostPopular.sort(sortByLikes);
-    console.log(mostPopular);
+    console.log(costumes);
+    costumes.sort(sortByLikes);
   }
+
+  // function sortPopular(response){
+  //   mostPopular = [];
+  //   for (i = 0; i < 10; i++){
+  //     mostPopular.push(response.data[i]);
+  //   };
+  //   mostPopular.sort(sortByLikes);
+  //   console.log(mostPopular);
+  // }
 
   function sortByLikes(a, b){
     var aCount = a.likes.count;
@@ -49,13 +62,13 @@
       var $item = $(item);
       var $img = $('<img>');
       var position = i + 1;
-      var photoUrl = mostPopular[i].images.standard_resolution.url;
-      var caption = mostPopular[i].caption.text;
+      var photoUrl = costumes[i].images.standard_resolution.url;
+      var caption = costumes[i].caption.text;
       caption = caption.split("@", 2);
-      caption = caption[1].split(" ", 1);
+      caption = caption[0].split(" ", 1);
       $item.find('.username').text( '@' + caption[0] );
       $item.find('.rank').text( position );
-      $item.find('.likes').text( mostPopular[i].likes.count );
+      $item.find('.likes').text( costumes[i].likes.count );
       $img.addClass('img');
       $img.attr('src', photoUrl);
       $item.find('.photo').append($img);
@@ -67,7 +80,6 @@
   function query(){
     $('.instagram').instagram({
       userId      : 492006858,
-      // hash        : 'nashville',
       count       : 33,
       accessToken : '234553568.467ede5.bae2604e97df4b3ba61e37a6c41e2245',
       clientId    : 'bf7acb5d25b841a7ae168fc0fea11208'
@@ -81,13 +93,13 @@
     $('.bigImg').remove();
     var $option = $('<img>');
     var spot = counter + 1;
-    var imaj = mostPopular[counter].images.standard_resolution.url;
+    var imaj = costumes[counter].images.standard_resolution.url;
     // var capshun = mostPopular[counter].caption.text;
     // capshun = capshun.split("@", 2);
     // capshun = capshun[1].split(" ", 1);
     // $('.user').text( "@" + capshun[0] );
     $('.ranking').text( spot );
-    $('.lykes').text( mostPopular[counter].likes.count );
+    $('.lykes').text( costumes[counter].likes.count );
     $option.addClass('bigImg');
     $option.attr('src', imaj);
     $('#slider').append($option);
