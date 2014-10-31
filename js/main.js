64598cdee9ceb3c9c6b270dbb27b0c772d45d860
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-10-30 21:16:35 -0500

    sorting by likes

--- js/main.js
+++ js/main.js
@@ -1,53 +1,60 @@
 (function($){
-
   'use strict';
-
   $(document).ready(initialize);
 
-  var $container = $('#container');
-
   function initialize(){
-    willLoad();
-    didLoad();
+    var $container = $('#container');
+    $container.isotope({
+      itemSelector : '.item',
+      layoutMode   : 'vertical',
+      getSortData  : {
+        likes    : '.likes'
+      }
+    });
+    // willLoad();
+    didLoad($container);
     query();
     setInterval(query, 5000);
   }
 
-  // function ajaxCall(){
-  //   // $.ajax({
-  //   //   type     : "GET",
-  //   //   dataType : "jsonp",
-  //   //   cache    : false,
-  //   //   url      : "https://api.instagram.com/v1/users/270865733/media/recent/?access_token=234553568.467ede5.bae2604e97df4b3ba61e37a6c41e2245",
-  //   //   success  : didLoad
-  //   // });
-  //   $container.isotope('updateSortData').isotope();
+  // function willLoad(){
+  //   $('.instagram').on('willLoadInstagram', function(event, options) {
+  //     console.log(options);
+  //   });
   // }
 
-  function willLoad(){
-    $('.instagram').on('willLoadInstagram', function(event, options) {
-      console.log(options);
-    });
-  }
-
-  function didLoad(){
+  function didLoad(dom){
     $('.instagram').on('didLoadInstagram', function(event, response) {
-      console.log(response);
-
       var mostPopular = [];
-      for (var i = 0; i < 10; i++) {
+      var i;
+      for (i = 0; i < 10; i++) {
         mostPopular.push(response.data[i]);
       };
       mostPopular.sort(sortByLikes);
       console.log(mostPopular);
 
       var likes = [];
-      for (var i = 0; i < 10; i++) {
+      for (i = 0; i < 10; i++) {
         likes.push(mostPopular[i].likes.count);
       };
       console.log(likes);
 
-      gridIt();
+      var $items = dom.find('.item');
+      var $item;
+      var $img;
+      var $photoUrl;
+      $items.each( function( i, item ) {
+        $item = $(item);
+        $item.find('.likes').text( mostPopular[i].likes.count );
+        $item.find('.username').text( mostPopular[i].user.username );
+        $photoUrl = mostPopular[i].images.standard_resolution.url;
+        $img = $('<img>');
+        $img.addClass('img');
+        $img.attr('src', $photoUrl);
+        $item.find('.photo').append($img);
+      });
+      dom.isotope('updateSortData', $items);
+      dom.isotope({ sortBy: 'likes' });
     });
   }
 
@@ -58,24 +65,13 @@
   }
 
   function query(){
+    console.log('query yo!');
     $('.instagram').instagram({
-      // userId      : 270865733,
-      hash        : 'nashville',
+      userId      : 270865733,
+      // hash        : 'nashville',
       count       : 10,
       accessToken : '234553568.467ede5.bae2604e97df4b3ba61e37a6c41e2245',
       clientId    : 'bf7acb5d25b841a7ae168fc0fea11208'
-    });
-  }
-
-  function gridIt(){
-    console.log("grid refresh");
-    $container.isotope({
-      getSortData  : {
-        likes      : '.likes'
-      },
-      itemSelector : '.item',
-      layoutMode   : 'vertical',
-      sortBy       : 'likes'
     });
   }
 
